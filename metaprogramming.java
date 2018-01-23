f95b55deb46750678ffa0997ef5a4ba5fbbb6a4d
AlexRutledge1 <31017536+AlexRutledge1@users.noreply.github.com>
2018-01-23 11:22:58 -0500

    Add files via upload

---
+++ metaprogramming.java
@@ -0,0 +1,37 @@
+import java.io.PrintWriter;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class metaprogramming {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		PrintWriter out = new PrintWriter(System.out);
+
+		TreeMap<String, Integer> map = new TreeMap<>();
+
+		while (scan.hasNext()) {
+			String a = scan.next();
+			if (a.equals("define")) {
+				int b = scan.nextInt();
+				map.put(scan.next(), b);
+			} else if (a.equals("eval")) {
+				String b = scan.next();
+				char c = scan.next().charAt(0);
+				String d = scan.next();
+				if (map.containsKey(b) && map.containsKey(d)) {
+					int e = map.get(b), f = map.get(d);
+					if (c == '=')
+						out.println((e == f) ? "true" : "false");
+					else if (c == '<')
+						out.println((e < f) ? "true" : "false");
+					else
+						out.println((e > f) ? "true" : "false");
+				} else
+					out.println("undefined");
+			}
+		}
+		out.close();
+		scan.close();
+	}
+}
