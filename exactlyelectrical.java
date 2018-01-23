f257f6f1a6523ea5db2005c819a1b6c99d1107b3
DanielW48 <31013771+DanielW48@users.noreply.github.com>
2018-01-23 11:30:52 -0500

    Add files via upload

---
+++ exactlyelectrical.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class exactlyelectrical {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int x1 = sc.nextInt(), y1 = sc.nextInt(), x2 = sc.nextInt(), y2 = sc.nextInt(), v = sc.nextInt();
+		int d = Math.abs(x1 - x2) + Math.abs(y1 - y2);
+		if(v < d || (v - d) % 2 == 1) System.out.println("N");
+		else System.out.println("Y");
+	}
+}
---
+++ froshweek.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class froshweek {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt(), m = sc.nextInt();
+		int[] tasks = new int[n], quiets = new int[m];
+		for(int i = 0; i < n; i++) tasks[i] = sc.nextInt();
+		Arrays.sort(tasks);
+		for(int i = 0; i < m; i++) quiets[i] = sc.nextInt();
+		Arrays.sort(quiets);
+		int currI = m - 1, out = 0;
+		for(int i = n - 1; i >= 0; i--){
+			if(quiets[currI] >= tasks[i]){
+				out++;
+				currI--;
+				if(currI < 0) break;
+			}
+		}
+		System.out.println(out);
+	}
+}
