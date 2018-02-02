0357718e29b7a02c9c3071c7c9eba516835c44d7
DanielW48 <31013771+DanielW48@users.noreply.github.com>
2018-02-02 07:51:24 -0500

    Update disastrousdowntime.java
    There we go, It has been accepted

--- disastrousdowntime.java
+++ disastrousdowntime.java
@@ -1,19 +1,23 @@
 //Daniel West
 import java.util.Scanner;
-public class disastrousdowntime {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt(), k = sc.nextInt();
-		int minServers = 1;
-		int[] servs = new int[n];
-		for(int i = 0; i < n; i++) servs[i] = sc.nextInt();
-		for(int i = 0; i < n; i++){
-			int temp = 1;
-			for(int j = i + 1; j < n && servs[j] - servs[i] < 1000; j++){
-				temp++;
-			}
-			minServers = Math.max(minServers, (int) Math.ceil(1.0 *temp / k));
-		}
-		System.out.println(minServers);
-	}
+public class Main {
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(), k = sc.nextInt(), out = 1;
+        int[] times = new int[n];
+        int firstIndex = 0, tempOut = 1;
+        times[0] = sc.nextInt();
+        for(int i = 1; i < n; i++){
+            times[i] = sc.nextInt();
+            while(times[firstIndex] + 1000 <= times[i]){
+                firstIndex++;
+                tempOut--;
+            }
+            tempOut++;
+            out = Math.max(out, tempOut);
+        }
+//      System.out.println("Max num requests at one time: " + out);
+        System.out.println((int) Math.ceil(1.0*out / k));
+        
+    }
 }
